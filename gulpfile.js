be5e695ebe764106f31772cade21e480619bf99c
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-02-14 15:24:51 +0000

    añadiendo task build a gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require("gulp");
 var shell = require('gulp-shell')
 var ghPages = require('gulp-gh-pages');
- 
+
 
 
 gulp.task('gh-pages', function() {
    return gulp.src('./gh-pages/**/*')
    .pipe(ghPages());
 });
+
+gulp.task('build',shell.task([
+  'npm run build'
+
+]));
